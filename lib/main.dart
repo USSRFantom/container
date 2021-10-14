c705f0adf664781e677ef07cd74e8ee62aff8284
UssrFantom <dimkakrohin@mail.ru>
2021-10-14 10:23:33 +0300

    Container

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main () => runApp(MyApp());
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Container"),
+          title: const Text("Container"),
           centerTitle: true,
         ),
-        body: Center(
-          child: Text('Center'),
+        body: Container( //Сам виджет контейнер
+          decoration: BoxDecoration(  //виджет бокс в котоом можно изменять цвет фон, делать бордер и т.д.
+            color: Colors.amberAccent, //устанавливаем значение цвета
+            border: Border.all(), //устанавливаем вордер
+          ),
+          width: 200,  //размер контейнера
+          height: 100, //размер контейнера
+          padding: EdgeInsets.all(16),  //делаем отступы
+          margin: EdgeInsets.all(30), //делаем отступы  внутри
+          alignment: Alignment.center,  //выравниваем по центру
+          child: Text('Container'),
         ),
       ),
     );
